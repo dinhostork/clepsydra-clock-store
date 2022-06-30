b37901a7e5bd60125e6c259cc623d2b84829216c
Dinho Stork <dinhostork@live.com>
2022-06-30 00:25:19 -0300

    feat: return all products

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('products')
 export class ProductController {
+  constructor(private readonly productService: ProductService) {}
   @Get()
   all() {
-    return `this will return all products`;
+    return this.productService.all();
   }
 }
